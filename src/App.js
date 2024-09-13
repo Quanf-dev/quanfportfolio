384b95e30416622c8dbb7206df9e4ad3dd0404b7
fomandquan <fomandquan123@gmail.com>
2024-09-13 20:49:43 +0700

    lastdeploy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import Topbar from "./components/topbar/Topbar";
 import "./app.scss"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, R } from "react-router-dom";
 import { useState } from "react";
-import { HashRouter } from "react-router-dom";
+
 import Fullpage from "./FullPage"
-import Home  from "./components/Home/Home";
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
-    <div>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={ <div className="top">
-             <Topbar/>
-            <div className="container">
-            <Fullpage/>
-            </div>
-            </div>
-           } />
-      </Routes>
-    </HashRouter>
+    <div className="top">
+     <Topbar/>
+     <div className="container">
+       <Fullpage/>
+     </div>
     </div>
-  
   );
 }
 
